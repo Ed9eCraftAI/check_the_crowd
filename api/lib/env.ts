6673e84f5ef6aef52e9dd88e7a65ce66b7e29c17
taeeun <kto416@kr.doubledown.com>
2026-02-24 23:31:55 +0900

    Update env.ts

--- api/lib/env.ts
+++ api/lib/env.ts
@@ -7,17 +7,6 @@ type EnvKey =
   | "DIRECT_URL"
   | "NODE_ENV";
 
-const ENV: Record<EnvKey, string | undefined> = {
-  CHECK_THE_CROWD_WALLETCONNECT_PROJECT_ID:
-    process.env.CHECK_THE_CROWD_WALLETCONNECT_PROJECT_ID,
-  CHECK_THE_CROWD_X_ACCOUNT: process.env.CHECK_THE_CROWD_X_ACCOUNT,
-  CHECK_THE_CROWD_APP_URL: process.env.CHECK_THE_CROWD_APP_URL,
-  CHECK_THE_CROWD_SESSION_SECRET: process.env.CHECK_THE_CROWD_SESSION_SECRET,
-  DATABASE_URL: process.env.DATABASE_URL,
-  DIRECT_URL: process.env.DIRECT_URL,
-  NODE_ENV: process.env.NODE_ENV,
-};
-
 export function env(key: EnvKey): string | undefined {
-  return ENV[key];
+  return process.env[key];
 }
